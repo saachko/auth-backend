765e759df61f5de9cf04fff0c9deee2cc549beb1
Anastasiya Sachko <naztya12323@gmail.com>
2023-01-20 01:58:47 +0300

    feat: create userScheme

---
+++ src/models/user.ts
@@ -0,0 +1,21 @@
+import mongoose from 'mongoose';
+
+const { Schema } = mongoose;
+const userScheme = new Schema({
+  username: {
+    type: String,
+    unique: true,
+    required: true,
+  },
+  email: {
+    type: String,
+    unique: true,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+});
+
+export default mongoose.model('User', userScheme);
