faafa4cb5e642d081ca0c6915112a37e4421b2b2
Anastasiya Sachko <naztya12323@gmail.com>
2023-01-20 22:05:32 +0300

    feat: add access token

--- src/routers/usersRouter.ts
+++ src/routers/usersRouter.ts
@@ -1,9 +1,10 @@
 import express from 'express';
 
 import { getUsers } from '../controllers/usersController';
+import usersMiddleware from '../middlewares/usersMiddlewares';
 
 const usersRouter = express.Router();
-usersRouter.get('/users', getUsers);
+usersRouter.get('/users', usersMiddleware, getUsers);
 usersRouter.delete('/:id');
 
 export default usersRouter;
